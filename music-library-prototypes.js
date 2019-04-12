9b13c756270d126ee9c8ec0ba9af4f31110389b7
Tess <tess.thornley@gmail.com>
2019-04-12 18:38:45 +0000

    prototypes added

--- music-library-prototypes.js
+++ music-library-prototypes.js
@@ -1,93 +1,72 @@
-var library = {
-  tracks: { t01: { id: "t01",
-                   name: "Code Monkey",
-                   artist: "Jonathan Coulton",
-                   album: "Thing a Week Three" },
-            t02: { id: "t02",
-                   name: "Model View Controller",
-                   artist: "James Dempsey",
-                   album: "WWDC 2003"},
-            t03: { id: "t03",
-                   name: "Four Thirty-Three",
-                   artist: "John Cage",
-                   album: "Woodstock 1952"}
-          },
-  playlists: { p01: { id: "p01",
-                      name: "Coding Music",
-                      tracks: ["t01", "t02"]
-                    },
-               p02: { id: "p02",
-                      name: "Other Playlist",
-                      tracks: ["t03"]
-                    }
-             },
-  printPlaylists: function() {
-    for(var playlist in this.playlists) {
-      var playlistnumber = this.playlists[playlist].id;
-      var playlistname = this.playlists[playlist].name;
-      var tracknumbers = this.playlists[playlist].tracks.length;
-    console.log(playlistnumber + ': ' + playlistname + ' - ' + tracknumbers + ' tracks')
-  }
-},
-  printTracks: function() {
-    for (var track in this.tracks) {
-      var tracknumber = this.tracks[track].id
-      var trackname = this.tracks[track].name
-      var artistname = this.tracks[track].artist
-      var album = this.tracks[track].album
-    console.log(tracknumber + ': ' + trackname + ' by ' + artistname + ' (' + album + ')')
-  }
-},
-  printPlaylist: function(playlistId) {
-    var playlistnumber = this.playlists[playlistId].id
-    var playlistname = this.playlists[playlistId].name
-    var tracknumbers = this.playlists[playlistId].tracks.length
+const Library = function(name, creator) {
+  this.name = name;
+  this.creator = creator;
+  this.playlists = [];
+  return this;
+};
 
-     console.log(playlistnumber + ': ' + playlistname + ' - ' + tracknumbers + ' tracks')
+Library.prototype.addPlaylists = function(playlist) {
+  this.playlists.unshift(playlist);
+};
 
-    for (var track of this.playlists[playlistId].tracks) {
-     var tracknumber = this.tracks[track].id
-     var trackname = this.tracks[track].name
-     var artistname = this.tracks[track].artist
-     var album = this.tracks[track].album
+const Playlists = function(name) {
+  this.name = name;
+  this.tracks = [];
+  return this;
+};
 
-     console.log(tracknumber + ': ' + trackname + ' by ' + artistname + ' (' + album + ')')
-  }
-  },
-  addTrackToPlaylist: function(trackId, playlistId) {
-    var playlisttoadd = this.playlists[playlistId]
-    playlisttoadd.tracks.push(trackId)
-    console.log(playlisttoadd.tracks)
-  },
+Playlists.prototype = Object.create(Library.prototype);
+Playlists.prototype.constructor = Playlists;
 
-  uid: function() {
-    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  },
-
-  addTrack: function(name, artist, album) {
-    var newtrackid = this.uid()
-
-    this.tracks[newtrackid] = { id: newtrackid,
-                   name: name,
-                   artist: artist,
-                   album: album }
-    console.log(this.tracks)
-  },
-  addPlaylist: function(name) {
-    var newplaylistid = this.uid()
-
-    this.playlists[newplaylistid] = { id: newplaylistid,
-                      name: name,
-                      tracks: []
-                    }
-    console.log(this.playlists)
-  }
+Playlists.prototype.addTracks = function(track) {
+  this.tracks.unshift(track);
 }
 
-library.printPlaylists()
-library.printTracks()
-library.printPlaylist('p01')
-library.addTrackToPlaylist('t03', 'p01')
-library.addTrack('Habitat', 'Tennyson', 'Tennyson')
-library.addPlaylist('Tess Playlist')
+Playlists.prototype.overallRating = function(ratings) {
+  let average = 0;
+  let length = 0;
+    for (let rating of ratings) {
+      for (let innerRating of rating) {
+        average += innerRating.rating;
+        length = rating.length;
+    };
+  };
+  return average / length;
+}
 
+Playlists.prototype.totalDuration = function(durations) {
+  let total = 0;
+  for (let duration of durations) {
+    for (let innerDuration of duration) {
+      total += innerDuration.songLength;
+    };
+  };
+  return total;
+}
+
+const Tracks = function(title, rating, songLength) {
+  this.title = title;
+  this.rating = rating;
+  this.songLength = songLength;
+  return this;
+};
+
+Tracks.prototype = Object.create(Playlists.prototype);
+Tracks.prototype.constructor = Tracks;
+
+
+let newLibrary = new Library('Tess Library', 'Tess'); 
+
+let newPlaylist = new Playlists('Tess Playlist');
+
+let songOne = new Tracks('Song one', 5, 180);
+let songTwo = new Tracks('Song two', 3, 200);
+let songThree = new Tracks('Song three', 5, 250);
+
+newLibrary.addPlaylists(newPlaylist);
+
+newPlaylist.addTracks([songOne, songTwo, songThree]);
+newPlaylist.overallRating(newPlaylist.tracks);
+newPlaylist.totalDuration(newPlaylist.tracks);
+
+console.log(newLibrary);
